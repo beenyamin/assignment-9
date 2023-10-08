83445594e3d41a6a1d03ffda3c3d9b8aadf9f07c
beenyamin <beenyaminkhan@gmail.com>
2023-10-08 12:54:23 +0600

    third

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -1,12 +1,17 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider/AuthProvider";
+import { FcGoogle } from "react-icons/fc";
+import toast, { Toaster } from "react-hot-toast";
+
 
 
 const Login = () => {
 
     const { loginUser, googlePopUp } = useContext(AuthContext);
-    const navigate = useNavigate();
+
+    const [loginError, setLoginError] = useState('');
+    const [loginSuccess, setLoginSuccess] = useState('');
 
     const handleLogin = e => {
         e.preventDefault();
@@ -14,14 +19,29 @@ const Login = () => {
         const password = e.target.password.value;
         console.log(email, password);
 
+        setLoginError('');
+        setLoginSuccess('');
+
+
+        if (password.length < 6) {
+            toast.error('Password should be at least 6 characters!')
+            return;
+
+        }
+        else if (!/[A-Z]/.test(password)) {
+            toast.error(" Your password should have  at Least one Capital letter! ")
+            return
+        }
+
         loginUser(email, password)
             .then(result => {
                 console.log(result.user)
-                e.target.reset();
-                navigate('/');
+                toast.success(' SuccessFully Log in!')
+ 
             })
             .catch(error => {
                 console.error(error)
+                toast.error('Login failed. Please check your Email And Password');
             })
     }
 
@@ -29,41 +49,68 @@ const Login = () => {
         googlePopUp()
             .then(result => {
                 console.log(result.user)
+                toast.success('Successfully logged in with Google!');
             })
             .catch(error => {
                 console.error(error)
+                toast.error('Google login failed. Please try again.');
             })
     }
 
 
     return (
 
-        <div className="w-8/12">
+        <div className="item-center justify-center">
 
-            <div className=" text-center justify-center my-20 bg-green-50  rounded py-4 " >
-                <h2 className="text-3xl font-bold my-10 text-center">Please Login</h2>
-                <form onSubmit={handleLogin} className=" md:w-3/4 lg:w-1/2 mx-auto">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="email" required name="email" placeholder="Email" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" required name="password" placeholder="Password" className="input input-bordered" />   
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
-                    </div>
-                </form>
-                <p className="text-center mt-4">Do not have an account? Please <Link className= " underline text-pink-400  font-bold" to={'/register'}>Register</Link></p>
+            <div className=" text-center">
 
-                <button onClick={handleGoogleLogin} className="bg-pink-400 px-4 rounded ">Google</button>
+                <div className="  my-20 rounded py-4 " >
+                    <h2 className="text-3xl font-bold my-10 text-center">Please Login</h2>
+
+                    <form onSubmit={handleLogin} className=" md:w-1/4 lg:w-1/2 mx-auto ">
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" required name="email" placeholder="Email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" required name="password" placeholder="Password" className="input input-bordered" />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="w-full text-white font-medium py-2 rounded bg-blue-700">Login</button>
+                        </div>
+
+                        {
+
+                           loginError && <p className="bg-red-400 my-5 font-semibold">{  loginError}</p>
+
+                        }
+
+                        {
+
+                            loginSuccess && <p className="text-blue-500 my-5 font-semibold">{loginSuccess}</p>
+
+                        }
+
+
+
+                    </form>
+
+
+                    <p className="text-center mt-4">Do not have an account? Please <Link className=" underline text-red-400   font-bold" to={'/register'}>Register</Link></p>
+
+                    <button onClick={handleGoogleLogin} className="bg-red-400 px-4 font-semibold text-white rounded"><FcGoogle className=""></FcGoogle>Google</button>
+
+                </div>
 
             </div>
+
+            <Toaster></Toaster>
 
 
         </div>
--- src/Components/Pages/Register.jsx
+++ src/Components/Pages/Register.jsx
@@ -1,11 +1,18 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider/AuthProvider";
+import toast, { Toaster } from "react-hot-toast";
+import { FcGoogle } from "react-icons/fc";
 
 
 const Register = () => {
 
-    const {newUser} = useContext(AuthContext);
+    const { googlePopUp } = useContext(AuthContext);
+
+    const [registerError, setRegisterError] = useState('');
+    const [signUpSuccess, setSignUpSuccess] = useState('');
+
+    const { newUser } = useContext(AuthContext);
 
     const handleRegister = e => {
         e.preventDefault();
@@ -14,26 +21,62 @@ const Register = () => {
         const password = e.target.password.value;
         console.log(name, email, password);
 
+        setRegisterError('');
+        setSignUpSuccess('');
+
+
+        if (password.length < 6) {
+          toast.error('Password should be at least 6 characters!')
+            return;
+
+        }
+        else if (!/[A-Z]/.test(password)) {
+
+            toast.error(" Your password should have  at Least one Capital letter! ")
+            return
+        }
+
         newUser(email, password)
             .then(result => {
                 console.log(result.user)
+                toast.success(' SuccessFully Signed Up!')
             })
             .catch(error => {
                 console.error(error);
+                toast.error('Registration Failed. Please check your Info');
+
             })
     }
 
-   
+
+    const handleGoogleRegister = () => {
+        googlePopUp()
+            .then(result => {
+                console.log(result.user)
+                toast.success(' Google Registration Successful');
+            })
+            .catch(error => {
+                console.error(error)
+                toast.error('Google Registration failed. Please try again.');
+            })
+    }
+
+
+
+
 
     return (
         <div>
 
 
             <div className=" text-center justify-center my-20 bg-green-50  rounded py-4 " >
+
+
                 <h2 className="text-3xl font-bold my-10 text-center">Please Register</h2>
 
 
                 <form onSubmit={handleRegister} className=" md:w-3/4 lg:w-1/2 mx-auto">
+
                     <div className="form-control">
 
                         <label className="label">
@@ -54,20 +97,35 @@ const Register = () => {
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" required name="password" placeholder="Type Password" className="input input-bordered" />
-                     
+                        <input type="password" required name="password" placeholder="New Password" className="input input-bordered" />
+
                     </div>
                     <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
+                        <button className="w-full text-white font-medium py-2 rounded bg-blue-700">Register</button>
                     </div>
+
+
+                    {
+
+                        registerError && <p className="bg-red-400 my-5 font-semibold">{registerError}</p>
+
+                    }
+
+                    {
+
+                        signUpSuccess && <p className="text-blue-500 my-5 font-semibold">{signUpSuccess}</p>
+
+                    }
+
                 </form>
-                <p className="text-center mt-4">Already have an account? Please <Link className= " underline text-pink-400 font-bold" to="/login">Login</Link></p>
+                <p className="text-center mt-4">Already have an account? Please <Link className=" underline text-red-400 font-bold" to="/login">Login</Link></p>
+
+                <button onClick={handleGoogleRegister} className="bg-red-400 px-4 font-semibold text-white rounded"><FcGoogle className=""></FcGoogle>Google</button>
+
             </div>
 
-
-
-
-
+            <Toaster></Toaster>
+     
 
         </div>
     );
